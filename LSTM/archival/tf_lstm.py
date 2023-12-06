9ad22bfa5fccd9da103d31557fe2039c3809fbc7
njcad <134978649+njcad@users.noreply.github.com>
2023-12-05 19:28:51 -0800

    Update tf_lstm.py

--- LSTM/archival/tf_lstm.py
+++ LSTM/archival/tf_lstm.py
@@ -10,20 +10,14 @@ Authors: Nate Cadicamo, Jessica Yang, Riya Karumanchi, Ira Thawornbut
 
 Approach: use tensorflow library to implement LSTM.
 
-PROBLEM: need to create new instance of a model each time you train it. This file currently does not do that, but best_hp.py does.
-
 Results: 
-    epochs: 100, batch size: 20, optimizer: adam, error: MSE
-        TEST LOSS PLANT 1: 0.006391435395926237
-        TEST LOSS PLANT 2: 0.010809659957885742
+    epochs: 100, batch size: 20, optimizer: adam, num layers: 50, error: MSE
+        TEST LOSS PLANT 1: 0.006251214072108269
+        TEST LOSS PLANT 2: 0.012048345059156418
 """
 
-import pandas as pd
-import numpy as np
-import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
-from tensorflow.keras.optimizers import Adam
 import matplotlib.pyplot as plt
 import get_data as GD
 
@@ -37,23 +31,24 @@ weather2 = "../data/Plant_2_Weather_Sensor_Data.csv"
 X_train_1, y_train_1, X_val_1, y_val_1, X_test_1, y_test_1 = GD.sort_data(weather1, power1)
 X_train_2, y_train_2, X_val_2, y_val_2, X_test_2, y_test_2 = GD.sort_data(weather2, power2)
 
-# create the tensorflow keras LSTM model
-model = Sequential()
-features = ['DAILY_YIELD', 'MAX_AMBIENT_TEMP', 'MIN_AMBIENT_TEMP', 'MAX_IRRADIATION', 'MIN_IRRADIATION']
-sequence_length = 24
-model.add(LSTM(50, activation='relu', input_shape=(sequence_length, len(features))))
-model.add(Dense(len(features))) 
-
-# compile the model with adam optimizer and mean squared error (MSE) loss
-model.compile(optimizer='adam', loss='mse')
-
 
 
 ### PLANT 1 MODEL TRAINING AND EVALUATION ###
 
+
+# create the tensorflow keras LSTM model
+model1 = Sequential()
+features = ['DAILY_YIELD', 'MAX_AMBIENT_TEMP', 'MIN_AMBIENT_TEMP', 'MAX_IRRADIATION', 'MIN_IRRADIATION']
+sequence_length = 24
+model1.add(LSTM(50, activation='relu', input_shape=(sequence_length, len(features))))
+model1.add(Dense(len(features))) 
+
+# compile the model with adam optimizer and mean squared error (MSE) loss
+model1.compile(optimizer='adam', loss='mse')
+
 # train the model on plant 1; TODO experiment with different epochs and batch_size
-history_1 = model.fit(X_train_1, y_train_1, epochs=100, batch_size=20, validation_data=(X_val_1, y_val_1))
-test_loss_1 = model.evaluate(X_test_1, y_test_1)
+history_1 = model1.fit(X_train_1, y_train_1, epochs=100, batch_size=20, validation_data=(X_val_1, y_val_1))
+test_loss_1 = model1.evaluate(X_test_1, y_test_1)
 print(f"\nTEST LOSS PLANT 1: {test_loss_1}\n")
 
 # plot the training loss, validation loss, and test loss
@@ -69,9 +64,20 @@ plt.show()
 
 ### PLANT 2 MODEL TRAINING AND EVALUATION ###
 
+
+# create the tensorflow keras LSTM model
+model2 = Sequential()
+features = ['DAILY_YIELD', 'MAX_AMBIENT_TEMP', 'MIN_AMBIENT_TEMP', 'MAX_IRRADIATION', 'MIN_IRRADIATION']
+sequence_length = 24
+model2.add(LSTM(50, activation='relu', input_shape=(sequence_length, len(features))))
+model2.add(Dense(len(features))) 
+
+# compile the model with adam optimizer and mean squared error (MSE) loss
+model2.compile(optimizer='adam', loss='mse')
+
 # train the model on plant 2; TODO experiment with different epochs and batch_size
-history_2 = model.fit(X_train_2, y_train_2, epochs=100, batch_size=20, validation_data=(X_val_2, y_val_2))
-test_loss_2 = model.evaluate(X_test_2, y_test_2)
+history_2 = model2.fit(X_train_2, y_train_2, epochs=100, batch_size=20, validation_data=(X_val_2, y_val_2))
+test_loss_2 = model2.evaluate(X_test_2, y_test_2)
 print(f"\nTEST LOSS PLANT 2: {test_loss_2}\n")
 
 # plot the training loss, validation loss, and test loss
